950efd48a9b2a7f3f9144d2dbfdb4447f9db1470
adrianry <adrianry>
2019-09-14 23:35:34 +0200

    integration camunda

--- src/main/java/com/adi/application/Application.java
+++ src/main/java/com/adi/application/Application.java
@@ -3,12 +3,21 @@ package com.adi.application;
 import com.adi.application.entities.User;
 import com.adi.application.repositories.UserRepository;
 import java.util.stream.Stream;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@EnableProcessApplication("adisProcessApplication")
 public class Application {
 
     public static void main(String[] args) {
@@ -24,6 +33,16 @@ public class Application {
             });
             userRepository.findAll().forEach(System.out::println);
         };
+    }
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        System.out.println("Starte Prozess...");
+        ProcessInstance instance = runtimeService.startProcessInstanceByKey("test1");
+        System.out.println(instance.getId());
     }
 
 }
